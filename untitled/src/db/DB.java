e0b69354271d6074a7ae534dbdc6264019461ffe
Walisson Santos <walissonsantost008@gmail.com>
2025-04-28 20:41:45 -0300

    Add metodos para fechamento do Statement e do ResultSet

--- untitled/src/db/DB.java
+++ untitled/src/db/DB.java
@@ -2,9 +2,7 @@ package db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class DB {
@@ -25,7 +23,7 @@ public class DB {
         return conn;
     }
 
-    public void closeConnection(){
+    public static void closeConnection(){
         if (conn != null){
             try {
                 conn.close();
@@ -44,6 +42,28 @@ public class DB {
         }
         catch (IOException e){
             throw new DbException(e.getMessage());
+        }
+    }
+
+    public static void closeStatement(Statement st){
+        if (st != null){
+            try {
+                st.close();
+            }
+            catch (SQLException e){
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs){
+        if (rs != null){
+            try {
+                rs.close();
+            }
+            catch (SQLException e){
+                throw new DbException(e.getMessage());
+            }
         }
     }
 }
