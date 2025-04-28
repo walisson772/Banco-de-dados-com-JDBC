0b8aeee3b2971abc96d414f8d29aa61c17b63f42
Walisson Santos <walissonsantost008@gmail.com>
2025-04-28 20:41:08 -0300

    Pegando dados do meu data base

--- untitled/src/aplication/Program.java
+++ untitled/src/aplication/Program.java
@@ -3,13 +3,32 @@ package aplication;
 import db.DB;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
     public static void main(String[] args) {
 
-        Connection conn = DB.getConn();
-        System.out.println("Conectou");
-        DB.closeConnection();
-        System.out.println("Fechou a conexão");
+        Connection conn = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+            conn = DB.getConn();
+            statement = conn.createStatement();
+
+            resultSet = statement.executeQuery("SELECT * FROM department");
+            while (resultSet.next()){
+                System.out.println(resultSet.getInt("Id") + ", " + resultSet.getString("Name"));
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeStatement(statement);
+            DB.closeResultSet(resultSet);
+            DB.closeConnection();
+        }
     }
 }
