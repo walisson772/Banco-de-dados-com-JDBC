07dcd7f0a40393afecb67fb9fcb750f8874d9df6
Walisson Santos <walissonsantost008@gmail.com>
2025-04-29 22:56:24 -0300

    Aprendendo a pegar dados do meu data base usando o java

---
+++ untitled/src/aplication/RecuperarDados.java
@@ -0,0 +1,36 @@
+package aplication;
+
+import db.DB;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class RecuperarDados {
+    public class Program {
+        public static void main(String[] args) {
+
+            Connection conn = null;
+            Statement statement = null;
+            ResultSet resultSet = null;
+            try {
+                conn = DB.getConn();
+                statement = conn.createStatement();
+
+                resultSet = statement.executeQuery("SELECT * FROM department");
+                while (resultSet.next()){
+                    System.out.println(resultSet.getInt("Id") + ", " + resultSet.getString("Name"));
+                }
+            }
+            catch (SQLException e) {
+                e.printStackTrace();
+            }
+            finally {
+                DB.closeStatement(statement);
+                DB.closeResultSet(resultSet);
+                DB.closeConnection();
+            }
+        }
+    }
+}
